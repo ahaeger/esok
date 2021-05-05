a375f78143674d1322f1d065452d7547e8a4fb31
Alexander Häger <haeger@spotify.com>
2021-05-05 12:40:53 +0200

    Initial commit.
    Barebones project layout.

---
+++ src/esok/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.0.0"
+
+__title__ = "esok"
+__description__ = "A CLI for Elasticsearch"
+__url__ = "https://github.com/ahaeger/esok"
+__doc__ = __description__ + " <" + __url__ + ">"
+
+__author__ = "Alexander Häger"
+__email__ = "me@haeger.me"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2021 Alexander Häger"
